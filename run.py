11640e0f2b44ce766695e774c705027b6e2c1cb9
danielammmm <danidanidjma@gmail.com>
2025-01-21 10:54:49 -0600

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     server_address = ('', 5000)
     httpd = server_class(server_address, handler_class)
     print("Server running on port 5000...")
-    httpd.serve_forever()hnmk 
+    httpd.serve_forever()
 
 if __name__ == "__main__":
     run()
